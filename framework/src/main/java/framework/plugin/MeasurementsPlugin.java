e2431fa7c9a333c96a02b335c0ec92ee81ddd685
Tomas Lestyan <tomas.lestyan@gmail.com>
2017-05-11 22:15:10 +0200

    settings test

--- framework/src/main/java/framework/plugin/MeasurementsPlugin.java
+++ framework/src/main/java/framework/plugin/MeasurementsPlugin.java
@@ -4,16 +4,16 @@
  */
 package main.java.framework.plugin;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.api.Plugin;
+import org.sonar.api.config.Settings;
+
+import main.java.framework.api.metrics.MetricsRegister;
 import main.java.framework.db.Configuration;
 import main.java.framework.db.DataSourceProvider;
 import main.java.framework.db.SaveMetricsClient;
 import main.java.framework.db.SchemaManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.sonar.api.Plugin;
-
-import main.java.framework.api.metrics.MetricsRegister;
-import main.java.framework.db.SonarDbClient;
 
 /**
  * The plugin definition
@@ -24,14 +24,18 @@ public class MeasurementsPlugin implements Plugin {
 
 	/** The logger object */
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
-
 	/**
 	 * Constructor
+	 * @param settings 
 	 */
-	public MeasurementsPlugin() {
+	public MeasurementsPlugin(Settings settings) {
 		log.info("plugin created");
 		// get database configuration
-		Configuration configuration = Configuration.INSTANCE;
+		String dbUrl = settings.getString("sonar.jdbc.url");
+		String driver = settings.getString("sonar.jdbc.driverClassName");
+		String userName = settings.getString("sonar.jdbc.username");
+		String password = settings.getString("sonar.jdbc.password");
+		Configuration configuration = new Configuration(dbUrl, driver, userName, password);
 		DataSourceProvider.setConfiguration(configuration);
 
 
@@ -52,4 +56,5 @@ public class MeasurementsPlugin implements Plugin {
 	public void define(Context context) {
 		context.addExtensions(MetricsRegister.class, JavaChecks.class, Rules.class, MeasurementsSensor.class);
 	}
+
 }
